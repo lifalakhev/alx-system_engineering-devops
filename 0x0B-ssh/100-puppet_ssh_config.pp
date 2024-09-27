3811ee5b34db79330f62501b7ec38ca7e48067a0
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2024-09-27 13:34:56 -0700

    updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,11 @@
-#  Config SSH config file for automated passwordless connection using Puppet
-
-include stdlib
+# set up your client SSH configuration file so that you can connect to a server without typing a password
 file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
-}
-
+  ensure => 'present',
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+  }
 file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  replace => true,
-}
+  ensure => 'present',
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+  }
